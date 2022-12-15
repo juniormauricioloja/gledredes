a42e06c603977d14e8b2ee0f393a7188017f73a4
juniormauricioloja <jrloja2495@gmail.com>
2022-12-15 10:19:45 -0500

    v2.5

--- content/update/controller/updateController.php
+++ content/update/controller/updateController.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
 
 include "../model/updateModel.php";
 require 'vendor/autoload.php';
